3a3729f14b0513478bfc71cc8662318949725633
Tristan Nishimoto <tristan.nishimoto@unity3d.com>
2021-10-06 12:26:18 -0700

    Minor tweaks.

--- VFXUnityProject/Assets/VFXAssets/Tristan/Scripts/InitSceneParameters.cs
+++ VFXUnityProject/Assets/VFXAssets/Tristan/Scripts/InitSceneParameters.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class InitSceneParameters : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private Transform _cameraTarget;
+    [SerializeField] private Camera _cameraTarget;
     
     [SerializeField] private Light _mainLight;
     [SerializeField] private Light _lightTarget;
 
     private Vector3 storedCameraPosition;
     private Quaternion storedCameraRotation;
+    private float storedCameraFov;
 
     private bool storedLightEnabled;
 
@@ -19,10 +20,12 @@ public class InitSceneParameters : MonoBehaviour
     {
         storedCameraPosition = _camera.transform.position;
         storedCameraRotation = _camera.transform.rotation;
+        storedCameraFov = _camera.fieldOfView;
         storedLightEnabled = _mainLight.gameObject.activeInHierarchy;
         
-        _camera.transform.position = _cameraTarget.position;
-        _camera.transform.rotation = _cameraTarget.rotation;
+        _camera.transform.position = _cameraTarget.transform.position;
+        _camera.transform.rotation = _cameraTarget.transform.rotation;
+        _camera.fieldOfView = _cameraTarget.fieldOfView;
         _mainLight.gameObject.SetActive(_lightTarget.gameObject.activeInHierarchy);
     }
 
@@ -30,6 +33,7 @@ public class InitSceneParameters : MonoBehaviour
     {
         _camera.transform.position = storedCameraPosition;
         _camera.transform.rotation = storedCameraRotation;
+        _camera.fieldOfView = storedCameraFov;
         _mainLight.gameObject.SetActive(storedLightEnabled);
     }
 }
